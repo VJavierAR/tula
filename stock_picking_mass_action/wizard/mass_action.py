b64477d93c9497ed12ea335d92334e237dae7609
Cesar <kliko1989@gmail.com>
2021-01-21 00:47:48 -0600

    sesea

--- stock_picking_mass_action/wizard/mass_action.py
+++ stock_picking_mass_action/wizard/mass_action.py
@@ -12,28 +12,28 @@ class pickingDesasignar(TransientModel):
     estado = fields.Many2one('detalle.move','Detalle')
     cancel_backorder=fields.Boolean(default=True)
     
-    def confirm(self):
-    	self.picking_id.move_ids_without_package.write({'estado':self.estado})
-    	if(self.cancel_backorder):
-    		self.picking_id.action_cancel()
+	def confirm(self):
+		self.picking_id.move_ids_without_package.write({'estado':self.estado})
+		if(self.cancel_backorder):
+			self.picking_id.action_cancel()
 
 
 
 class BckorderCancel():
 	_inherit='stock_backorder_confirmation'
 
-    def process_cancel_backorder(self):
-        self._process(cancel_backorder=True)
-        w=self.env['picking.desasignar'].create({'picking_id':self.pick_ids,'cancel_backorder':False})
-        view=self.env.ref('stock.view_picking_desasignar')
-        return {
-                'name': _('Motivos de Cancelacion'),
-                'type': 'ir.actions.act_window',
-                'res_model': 'picking.desasignar',
-                'view_type': 'form',
-                'view_mode': 'form',
-                'res_id':w.id,
-                'views': [(view.id, 'form')],
-                'view_id': view.id,
-                'target':'new'
-            }
+	def process_cancel_backorder(self):
+		self._process(cancel_backorder=True)
+		w=self.env['picking.desasignar'].create({'picking_id':self.pick_ids,'cancel_backorder':False})
+		view=self.env.ref('stock.view_picking_desasignar')
+		return {
+				'name': _('Motivos de Cancelacion'),
+				'type': 'ir.actions.act_window',
+				'res_model': 'picking.desasignar',
+				'view_type': 'form',
+				'view_mode': 'form',
+				'res_id':w.id,
+				'views': [(view.id, 'form')],
+				'view_id': view.id,
+				'target':'new'
+				}
