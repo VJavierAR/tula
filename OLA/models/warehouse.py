9afcc6065a99d802682068b14417ec0ab4469967
Cesar <kliko1989@gmail.com>
2021-04-21 04:18:24 -0500

    dasd

--- OLA/models/warehouse.py
+++ OLA/models/warehouse.py
@@ -6,12 +6,11 @@ class almacen(models.Model):
 	code = fields.Char('Short Name', required=True, help="Short name used to identify your warehouse")
 
 class stock(models.Model):
-	_inherit = 'stock.picking'
-	#en_proceso=fields.Boolean(default=False)
-	state = fields.Selection(selection_add=[('printed', 'Impreso')])
+    _inherit = 'stock.picking'
+    state = fields.Selection(selection_add=[('printed', 'Impreso')])
     user_print_id = fields.Many2one(comodel_name="res.users", string="Usuario que imprimió", tracking=True, copy=False, required=False)
     user_validate_id = fields.Many2one(comodel_name="res.users", string="Usuario que validó", tracking=True, copy=False, required=False)
-	#@api.multi
+    
 	def print_vale_de_entrega(self):
 		if(self.state=='printed'):
 			raise exceptions.UserError('Orden de surtido ya se encunetra en proceso')			
