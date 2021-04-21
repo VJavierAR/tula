fa1254db67863754fb90d57b22476a4ac216f99c
Cesar <kliko1989@gmail.com>
2021-04-21 04:12:01 -0500

    dasd

--- OLA/models/warehouse.py
+++ OLA/models/warehouse.py
@@ -7,13 +7,15 @@ class almacen(models.Model):
 
 class stock(models.Model):
 	_inherit = 'stock.picking'
-	en_proceso=fields.Boolean(default=False)
+	#en_proceso=fields.Boolean(default=False)
+	state = fields.Selection(selection_add=[('printed', 'Impreso')])
     user_print_id = fields.Many2one(comodel_name="res.users", string="Usuario que imprimió", tracking=True, copy=False, required=False)
     user_validate_id = fields.Many2one(comodel_name="res.users", string="Usuario que validó", tracking=True, copy=False, required=False)
 	#@api.multi
 	def print_vale_de_entrega(self):
-		if(self.en_proceso):
+		if(self.state=='printed'):
 			raise exceptions.UserError('Orden de surtido ya se encunetra en proceso')			
 		else:
-			self.en_proceso=True
+			self.state='printed'
+			self.user_print_id=self.env.user.id
 			return self.env.ref("stock.action_report_picking").report_action(self)
