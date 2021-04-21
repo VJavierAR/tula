936c20b3022fc10541e463cf4568b8d90b208b66
Cesar <kliko1989@gmail.com>
2021-04-21 00:56:23 -0500

    dasd

--- OLA/models/product.py
+++ OLA/models/product.py
@@ -12,4 +12,4 @@ class product(models.Model):
 
 class productPr(models.Model):
 	_inherit = 'product.product'
-	sug_rel=fields.Many2many('product.product','product_may_sug')
+	sug_rel=fields.Many2many('product.product','product_may_sug',id1='id', id2='id')
