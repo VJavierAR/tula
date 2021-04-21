7bda0075bd9e0452b977f00e3ab23f05a2519595
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-04-21 03:17:28 -0500

    botón imprimir reporte de entrega

--- OLA/models/warehouse.py
+++ OLA/models/warehouse.py
@@ -4,3 +4,10 @@ from odoo import models, fields, api,_
 class almacen(models.Model):
 	_inherit='stock.warehouse'
 	code = fields.Char('Short Name', required=True, help="Short name used to identify your warehouse")
+
+class stock(models.Model):
+	_inherit = 'stock.picking'
+
+	@api.multi
+	def print_vale_de_entrega(self):
+		return self.env.ref("stock.report_deliveryslip").report_action(self)
