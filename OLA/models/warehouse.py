42fd4762e5a91555ae6d5224783cea5bb935aaf5
Cesar <kliko1989@gmail.com>
2021-04-21 03:28:57 -0500

    jgjj

--- OLA/models/warehouse.py
+++ OLA/models/warehouse.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api,_
-
+from odoo import exceptions
 
 class almacen(models.Model):
 	_inherit='stock.warehouse'
@@ -7,9 +7,12 @@ class almacen(models.Model):
 
 class stock(models.Model):
 	_inherit = 'stock.picking'
-	en_proceso=fields.Boolen(default=False)
+	en_proceso=fields.Boolean(default=False)
 
 	#@api.multi
 	def print_vale_de_entrega(self):
-		self.en_proceso=True
-		return self.env.ref("stock.report_deliveryslip").report_action(self)
+		if(self.en_proceso):
+			raise exceptions.UserError("No se puede validar la orden dado que el Cliente: "+self.cliente.name+' no cuenta con ubicaciones disponibles o no tiene ningun servicio contratado.')			
+		else:
+			self.en_proceso=True
+			return self.env.ref("stock.report_deliveryslip").report_action(self)
