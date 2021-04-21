e4fd030ab59ed73192a66911a65813704686bcd2
Cesar <kliko1989@gmail.com>
2021-04-21 03:25:25 -0500

    das

--- OLA/models/warehouse.py
+++ OLA/models/warehouse.py
@@ -7,7 +7,9 @@ class almacen(models.Model):
 
 class stock(models.Model):
 	_inherit = 'stock.picking'
+	en_proceso=fields.Boolen(default=False)
 
 	#@api.multi
 	def print_vale_de_entrega(self):
+		self.en_proceso=True
 		return self.env.ref("stock.report_deliveryslip").report_action(self)
