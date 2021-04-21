c24967a4dd5625b26a9e49d027311fbeb56d1b09
Cesar <kliko1989@gmail.com>
2021-04-21 01:29:21 -0500

    das

--- OLA/models/sale.py
+++ OLA/models/sale.py
@@ -10,9 +10,9 @@ class saleOr(models.Model):
 
 	@api.onchange('product_id')
 	def addSugges(self):
-		p=self.product_id.mapped('sug_rel.id2')
+		p=self.product_id.mapped('sug_rel.id')
 		for pi in p:
-			self.order_id.productos_sugeridos=[(0,0,{'product_rel':self.product_id.id,'product_sug':pi})]
+			self.order_id.productos_sugeridos.write({'product_rel':self.product_id.id,'product_sug':pi})
 
 
 class productSuggested(models.Model):
