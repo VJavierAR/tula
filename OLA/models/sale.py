f631105cdc1b4e97a3a8c09508bff253b554499d
Cesar <kliko1989@gmail.com>
2021-04-22 07:14:10 -0500

    dasd

--- OLA/models/sale.py
+++ OLA/models/sale.py
@@ -130,7 +130,8 @@ class productSuggested(models.Model):
 
 	@api.onchange('agregar')
 	def add(self):
-		self.rel_id.arreglo=str([self.product_sug.id])
+		if(self.agregar):
+			self.rel_id.write({'arreglo':str([self.product_sug.id])})
 
 	def add1(self):
 		self.rel_id.order_line=[(0, 0, {'product_id':self.product_sug.id,'order_id':self.rel_id.id})]
