941ea7948d5af11677a741c88495da14269f5b0b
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-04-22 13:04:22 -0500

    Update sale.py

--- OLA/models/sale.py
+++ OLA/models/sale.py
@@ -59,7 +59,7 @@ class sale(models.Model):
 				if linea.price_unit < linea.x_studio_field_Ml1CB:
 					title = title + "Precio minímo de venta. | "
 					message = message + """El producto: """ + str(linea.product_id.display_name) + """ esta rebasando su precio minímo de venta.\nPrecio: """ + str(linea.price_unit) + """\nPrecio minímo: """ + str(linea.x_studio_field_Ml1CB) + """\n"""
- 					genero_alertas = True
+					genero_alertas = True
 				# raise Warning('Estas rebasando tu precio minímo de venta')
 				elif linea.price_subtotal < linea.x_studio_field_Ml1CB:
 					title = title + "Precio minímo de venta. | "
