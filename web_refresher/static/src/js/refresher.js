1bf09304609c0bcd561cbd3b38b7a285ca308022
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-04-23 11:46:12 -0500

    Update refresher.js

--- web_refresher/static/src/js/refresher.js
+++ web_refresher/static/src/js/refresher.js
@@ -4,6 +4,9 @@ odoo.define("refresher.pager", function(require) {
     "use strict";
 
     var pager = require("web.Pager");
+    var AbstractController = require('web.AbstractController');
+    var inter;
+
     pager.include({
         start: function() {
             var self = this;
@@ -23,20 +26,43 @@ odoo.define("refresher.pager", function(require) {
             console.log("self.__parentedParent.viewType: ")
             console.log(self.__parentedParent.viewType)
             //undefined
-            var inter;
+
             if (self.__parentedParent.viewType == "list") {
                 inter = setInterval(function() {
                     self._changeSelection(0);
                 }, 10000, "JavaScript");
             }
 
-            $(".o_data_row").on("click", function() {
+            /*
+            $(this).parents("tr").find(".o_data_row").on("click", function() {
                 console.log("Di clikc en una fila")
                 clearInterval(inter);
             });
-
+            */
             self.$el.prepend($button);
             return res;
+        },
+    });
+
+    AbstractController.include({
+        _onOpenRecord: function (event, params) {
+            event.stopPropagation();
+            /*
+            var record = this.model.get(event.data.id, {raw: true});
+            var view_type = 'form'
+            if (this.initialState.context.view_type) {
+                view_type = this.initialState.context.view_type
+            }
+            this.trigger_up('switch_view', {
+                view_type: view_type,
+                res_id: record.res_id,
+                mode: event.data.mode || 'readonly',
+                model: this.modelName,
+            });
+            */
+            console.log("Di clikc en una fila")
+            clearInterval(inter);
+
         },
     });
 });
