6cd34ac9742f1e632fe947b86c1ab9275c3a1243
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-04-28 13:01:41 -0500

    Renderizado automático en listas
    Al estar en una vista lista la pagina se renderiza cada 10 segundos

--- web_refresher/static/src/js/refresher.js
+++ web_refresher/static/src/js/refresher.js
@@ -5,7 +5,23 @@ odoo.define("refresher.pager", function(require) {
 
     var pager = require("web.Pager");
     var AbstractController = require('web.AbstractController');
+    var ListController = require('web.ListController')
+    var ListRenderer = require('web.ListRenderer')
+    var ListView = require('web.ListView')
     var inter;
+    var idPresupuestos = 806; //id de vista lista sale.order
+
+    // Allowed decoration on the list's rows: bold, italic and bootstrap semantics classes
+    var DECORATIONS = [
+        'decoration-bf',
+        'decoration-it',
+        'decoration-danger',
+        'decoration-info',
+        'decoration-muted',
+        'decoration-primary',
+        'decoration-success',
+        'decoration-warning'
+    ];
 
     pager.include({
         start: function() {
@@ -21,24 +37,19 @@ odoo.define("refresher.pager", function(require) {
                 self._changeSelection(0);
             });
 
-            console.log("self.__parentedParent: ")
-            console.log(self.__parentedParent)
-            console.log("self.__parentedParent.viewType: ")
-            console.log(self.__parentedParent.viewType)
+            //console.log("self.__parentedParent: ")
+            //console.log(self.__parentedParent)
+            //console.log("self.__parentedParent.viewType: ")
+            //console.log(self.__parentedParent.viewType)
             //undefined
-
+            /*
             if (self.__parentedParent.viewType == "list") {
                 inter = setInterval(function() {
                     self._changeSelection(0);
                 }, 10000, "JavaScript");
             }
-
-            /*
-            $(this).parents("tr").find(".o_data_row").on("click", function() {
-                console.log("Di clikc en una fila")
-                clearInterval(inter);
-            });
             */
+
             self.$el.prepend($button);
             return res;
         },
@@ -65,4 +76,77 @@ odoo.define("refresher.pager", function(require) {
 
         },
     });
+
+    ListController.include({
+        _onCreateRecord: function() {
+            console.log("Iniciando un formulario")
+            this._super.apply(this, arguments);
+            clearInterval(inter);
+            for (var i = 1; i < inter; i++)
+                window.clearInterval(i);
+        }
+    });
+
+    ListRenderer.include({
+        start: function () {
+            //this._super.apply(this, arguments);
+            var self = this;
+            var res = self._super();
+
+
+            return res;
+        },
+        _renderBody: function() {
+            this._super.apply(this, arguments);
+            var self = this;
+            var $rows = this._renderRows();
+            while ($rows.length < 4) {
+                $rows.push(self._renderEmptyRow());
+            }
+            //console.log("Renderizando cuerpo de lista.....");
+            if (self.__parentedParent.viewType == "list" && !inter) {
+                console.log("*****************Mi papa es una lista******************")
+                inter = setInterval(function(papa) {
+                    //console.log("intervalo de renderizado cada 10 seg iniciado...");
+                    //console.log(papa);
+                    papa.pager._changeSelection(0);
+                }, 10000, self.__parentedParent);
+            }
+            if (self.__parentedParent.viewType == "form") {
+                //console.log("*****************Mi papa es un formulario******************")
+                //Deten renderizado
+                clearInterval(inter);
+                for (var i = 1; i < inter; i++)
+                    window.clearInterval(i);
+                inter = 0;
+            }
+            return $('<tbody>').append($rows);
+        },
+        /**
+         * return the number of visible columns.  Note that this number depends on
+         * the state of the renderer.  For example, in editable mode, it could be
+         * one more that in non editable mode, because there may be a visible 'trash
+         * icon'.
+         *
+         * @private
+         * @returns {integer}
+         */
+        _getNumberOfCols: function () {
+            var n = this.columns.length;
+            return this.hasSelectors ? n + 1 : n;
+        },
+        /**
+         * Render a complete empty row.  This is used to fill in the blanks when we
+         * have less than 4 lines to display.
+         *
+         * @private
+         * @returns {jQueryElement} a <tr> element
+         */
+        _renderEmptyRow: function () {
+            var $td = $('<td>&nbsp;</td>').attr('colspan', this._getNumberOfCols());
+            return $('<tr>').append($td);
+        },
+
+    });
+
 });
