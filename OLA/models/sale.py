c47a7012734c57a5374b3d95518e663a4ca5ebf8
Cesar <kliko1989@hotmail.com>
2021-04-30 10:52:35 -0500

    sasa

--- OLA/models/sale.py
+++ OLA/models/sale.py
@@ -11,6 +11,28 @@ class sale(models.Model):
 	arreglo = fields.Char(default='[]')
 	urgencia = fields.Selection(selection=[('Urgente','Urgente'),('Muy urgente','Muy urgente')], string="Urgencia")
 
+    def action_confirm(self):
+        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+            raise UserError(_(
+                'It is not allowed to confirm an order in the following states: %s'
+            ) % (', '.join(self._get_forbidden_state_confirm())))
+
+        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+            order.message_subscribe([order.partner_id.id])
+        self.write({
+            'state': 'sale',
+            'date_order': fields.Datetime.now()
+        })
+
+        # Context key 'default_name' is sometimes propagated up to here.
+        # We don't need it and it creates issues in the creation of linked records.
+        context = self._context.copy()
+        context.pop('default_name', None)
+
+        self.with_context(context)._action_confirm()
+        if self.env.user.has_group('sale.group_auto_done_setting'):
+            self.action_done()
+        return True
 
 	@api.onchange('arreglo')
 	def addsegesst(self):
