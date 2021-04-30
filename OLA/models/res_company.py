564f71a42cb5d18ceb3f0a7fcbbc34a157e84451
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-04-30 13:55:32 -0500

    Update res_company.py

--- OLA/models/res_company.py
+++ OLA/models/res_company.py
@@ -6,3 +6,7 @@ _logger = logging.getLogger(__name__)
 class partner(models.Model):
     _inherit = 'res.company'
     auto_picking=fields.Boolean()
+
+    limite_credito_global = fields.Monetary(
+        string = "Límite de crédito global"
+    )
