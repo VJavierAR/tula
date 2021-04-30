65bb55f85ca10ce07494246b987985f8a38b335c
Cesar <kliko1989@hotmail.com>
2021-04-30 11:11:55 -0500

    dasdas

--- OLA/models/sale.py
+++ OLA/models/sale.py
@@ -19,28 +19,28 @@ class sale(models.Model):
         ('cancel', 'Cancelled'),
         ], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
 
-    def action_confirm(self):
-        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
-            raise UserError(_(
-                'It is not allowed to confirm an order in the following states: %s'
-            ) % (', '.join(self._get_forbidden_state_confirm())))
+	def action_confirm(self):
+	    if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+	        raise UserError(_(
+	            'It is not allowed to confirm an order in the following states: %s'
+	        ) % (', '.join(self._get_forbidden_state_confirm())))
 
-        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-            order.message_subscribe([order.partner_id.id])
-        self.write({
-            'state': 'sale',
-            'date_order': fields.Datetime.now()
-        })
+	    for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+	        order.message_subscribe([order.partner_id.id])
+	    self.write({
+	        'state': 'sale',
+	        'date_order': fields.Datetime.now()
+	    })
 
-        # Context key 'default_name' is sometimes propagated up to here.
-        # We don't need it and it creates issues in the creation of linked records.
-        context = self._context.copy()
-        context.pop('default_name', None)
+	    # Context key 'default_name' is sometimes propagated up to here.
+	    # We don't need it and it creates issues in the creation of linked records.
+	    context = self._context.copy()
+	    context.pop('default_name', None)
 
-        self.with_context(context)._action_confirm()
-        if self.env.user.has_group('sale.group_auto_done_setting'):
-            self.action_done()
-        return True
+	    self.with_context(context)._action_confirm()
+	    if self.env.user.has_group('sale.group_auto_done_setting'):
+	        self.action_done()
+	    return True
 
 	@api.onchange('arreglo')
 	def addsegesst(self):
