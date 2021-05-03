ff83b92aae03f6de921e8f7ee0266158cd52b3a8
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-05-03 04:30:54 -0500

    Update sale_order_wizard.py

--- OLA/wizard/sale_order_wizard.py
+++ OLA/wizard/sale_order_wizard.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, api,_
-from odoo.exceptions import exceptions, UserError
+from odoo.exceptions import UserError
+from odoo import exceptions
 import logging, ast
 _logger = logging.getLogger(__name__)
 
