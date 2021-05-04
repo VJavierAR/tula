2434d498ca0b08e475e16ae20877cf4a389eab2e
Cesar <kliko1989@hotmail.com>
2021-05-04 15:57:49 -0500

    vdffvdv

--- OLA/models/sale_order_line.py
+++ OLA/models/sale_order_line.py
@@ -12,16 +12,16 @@ class saleOr(models.Model):
 		default = False
 	)
 
-	@api.onchange('product_id')
-	def stock(self):
-		res={}
-		if(self.product_id.qty_available<=0):
-			pa=self.product_id.mapped('alt_rel.id')
-			po=self.env['product.product'].browse(pa)
-			po1=po.filtered(lambda x:x.qty_available>0)
-			if(po1.mapped('id')!=[]):
-				res['domain']={'product_id':[['id','in',po1.mapped('id')]]}
-				return res
+	# @api.onchange('product_id')
+	# def stock(self):
+	# 	res={}
+	# 	if(self.product_id.qty_available<=0):
+	# 		pa=self.product_id.mapped('alt_rel.id')
+	# 		po=self.env['product.product'].browse(pa)
+	# 		po1=po.filtered(lambda x:x.qty_available>0)
+	# 		if(po1.mapped('id')!=[]):
+	# 			res['domain']={'product_id':[['id','in',po1.mapped('id')]]}
+	# 			return res
 	# def addSugges(self):
 	# 	p=self.product_id.mapped('sug_rel.id')
 	# 	arreglo=[]
