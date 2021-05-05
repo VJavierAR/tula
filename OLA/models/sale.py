1fbfbcf95468e97f4964c309165ce9b78b7ee6c3
Cesar <kliko1989@hotmail.com>
2021-05-05 04:01:08 -0500

    DASDA

--- OLA/models/sale.py
+++ OLA/models/sale.py
@@ -144,7 +144,7 @@ class sale(models.Model):
 		for sug in self.productos_sugeridos:
 			if(sug.agregar==True):
 				if(sug.product_sug.id not in self.order_line.mapped('product_id.id')):
-					self.order_line={'product_id':sug.product_sug.id,'order_id':self.id}
+					self.order_line={'product_id':sug.product_sug.id}
 					#self.order_line=[{'product_id':sug.product_sug.id,'order_id':self.id,'product_uom_qty':sug.product_sug.uom_id.id,'name':sug.product_sug.description,'price_unit':sug.product_sug.lst_price}]
 					#self.write({'order_line':[(0, 0, {'product_id':sug.product_sug.id,'order_id':self.id,'product_uom_qty':sug.product_sug.uom_id.id,'name':sug.product_sug.description,'price_unit':sug.product_sug.lst_price})]})
 
