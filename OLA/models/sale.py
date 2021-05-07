4ed30b9ed0ad47e77be4fe2049ceb84dc2ca45d2
Cesar <kliko1989@hotmail.com>
2021-05-07 14:58:12 -0500

    hnggfg

--- OLA/models/sale.py
+++ OLA/models/sale.py
@@ -127,9 +127,9 @@ class sale(models.Model):
 					return pi.button_validate()
 					#pi._autoconfirm_picking()
 				if pi.state in ('waiting','confirmed'):
-					raise Warning(_('No hay stock para el pedido'))
+					#raise Warning(_('No hay stock para el pedido'))
 					#_logger.info(self.picking_ids.mapped('move_line_ids.state'))
-					#return {'warning': {'title': 'Sin stock','message': 'No hay stock'}}
+					return {'warning': {'title': _('Sin stock'),'message': _('No hay stock')}}
 
 
 						
