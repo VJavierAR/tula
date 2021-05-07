db06f6cae75f1af8c8a9c39ef8fb1dc51b960893
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-05-07 01:05:29 -0500

    a

--- OLA/models/sale_order_line.py
+++ OLA/models/sale_order_line.py
@@ -59,9 +59,10 @@ class saleOr(models.Model):
 	@api.onchange('product_id')
 	def buscaProductos(self):                                        
 	    ft=''
-	    cabecera='<table><tr><th>Compañia</th><th>Cantidad</th></tr>'
-	    for cal in self.product_id.sudo().stock_quant_ids.filtered(lambda x:x.company_id.id!=False and x.quantity>=0):   
-	        ft='<tr><td>'+str(cal.company_id.name)+'</td><td>'+str(cal.quantity)+'</td></tr>'+ft
+	    cabecera='<table><tr><th>Compañia</th><th>Bodega</th><th>Cantidad</th></tr>'
+	    for cal in self.product_id.sudo().stock_quant_ids.filtered(lambda x:x.company_id.id!=False and x.quantity>=0):
+
+	        ft='<tr><td>'+str(cal.company_id.name)+'</td><td>'+str(cal.location_id.display_name)+'</td><td>'+str(cal.quantity)+'</td></tr>'+ft
 	    tabla=cabecera+ft+'</table>'
 	    self.existencias=str(tabla)
 
