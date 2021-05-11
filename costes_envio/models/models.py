dfb37fca2fb6c45ce6ab00cc4d51fd1a1060e2a6
Cesar <kliko1989@hotmail.com>
2021-05-11 14:39:40 -0500

    deweedewe

---
+++ costes_envio/models/models.py
@@ -0,0 +1,96 @@
+-*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+SPLIT_METHOD = [
+    ('equal', 'Equal'),
+    ('by_quantity', 'By Quantity'),
+    ('by_current_cost_price', 'By Current Cost'),
+    ('by_weight', 'By Weight'),
+    ('by_volume', 'By Volume'),
+]
+
+
+class costes_envio(models.Model):
+    _inherit = 'stock.landed.cost'
+
+    def compute_landed_cost(self):
+        AdjustementLines = self.env['stock.valuation.adjustment.lines']
+        AdjustementLines.search([('cost_id', 'in', self.ids)]).unlink()
+
+        digits = self.env['decimal.precision'].precision_get('Product Price')
+        towrite_dict = {}
+        for cost in self.filtered(lambda cost: cost.picking_ids):
+            total_qty = 0.0
+            total_cost = 0.0
+            total_weight = 0.0
+            total_volume = 0.0
+            total_line = 0.0
+            all_val_line_values = cost.get_valuation_lines()
+            for val_line_values in all_val_line_values:
+                for cost_line in cost.cost_lines:
+                    _logger.info(str(val_line_values))
+                    val_line_values.update({'cost_id': cost.id, 'cost_line_id': cost_line.id})
+                    self.env['stock.valuation.adjustment.lines'].create(val_line_values)
+                total_qty += val_line_values.get('quantity', 0.0)
+                total_weight += val_line_values.get('weight', 0.0)
+                total_volume += val_line_values.get('volume', 0.0)
+
+                former_cost = val_line_values.get('former_cost', 0.0)
+                # round this because former_cost on the valuation lines is also rounded
+                total_cost += tools.float_round(former_cost, precision_digits=digits) if digits else former_cost
+
+                total_line += 1
+
+            for line in cost.cost_lines:
+                value_split = 0.0
+                for valuation in cost.valuation_adjustment_lines:
+                    value = 0.0
+                    if valuation.cost_line_id and valuation.cost_line_id.id == line.id:
+                        if line.split_method == 'by_quantity' and total_qty:
+                            per_unit = (line.price_unit / total_qty)
+                            value = valuation.quantity * per_unit
+                        elif line.split_method == 'by_weight' and total_weight:
+                            per_unit = (line.price_unit / total_weight)
+                            value = valuation.weight * per_unit
+                        elif line.split_method == 'by_volume' and total_volume:
+                            per_unit = (line.price_unit / total_volume)
+                            value = valuation.volume * per_unit
+                        elif line.split_method == 'equal':
+                            value = (line.price_unit / total_line)
+                        elif line.split_method == 'by_current_cost_price' and total_cost:
+                            per_unit = (line.price_unit / total_cost)
+                            value = valuation.former_cost * per_unit
+                        else:
+                            value=(valuation.former_cost*(int(line.split_method)/100))
+                        if digits:
+                            value = tools.float_round(value, precision_digits=digits, rounding_method='UP')
+                            fnc = min if line.price_unit > 0 else max
+                            value = fnc(value, line.price_unit - value_split)
+                            value_split += value
+
+                        if valuation.id not in towrite_dict:
+                            towrite_dict[valuation.id] = value
+                        else:
+                            towrite_dict[valuation.id] += value
+        for key, value in towrite_dict.items():
+            AdjustementLines.browse(key).write({'additional_landed_cost': value})
+        return True
+
+class LandedCostLine(models.Model):
+    _inherit = 'stock.landed.cost.lines'
+
+    def get_split(self):
+    	v=self.env['define.cost'].search([])
+    	t=[]
+    	for val in v:
+    		t.append((str(val.value),str(val.name)))
+    	arr=[]
+    	arr=SPLIT_METHOD+t
+
+    split_method = fields.Selection(get_split, string='Split Method', required=True)
+
+class defineCost(models.Model):
+	_name='define.cost'
+	_description='Definicion de costes adiccionales'
+	name=fields.Char('Descripción')
+	valor=fields.Integer('Valor')
