d39b0e55a6f5a6bfb31d9881d31c623bdc88babe
Gerardo <gerardoleyvat@gmail.com>
2021-05-18 13:59:30 -0500

    ghuntj

--- facturacion/__manifest__.py
+++ facturacion/__manifest__.py
@@ -28,6 +28,7 @@
         'views/views.xml',
         'views/templates.xml',
         'wizard/pagos_wizard.xml',
+        'views/account_move_mail.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
