0f20091da1dae91a8666d15ac4e50927961baa8e
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-05-24 14:09:53 -0500

    a

--- contactos_conexion/__manifest__.py
+++ contactos_conexion/__manifest__.py
@@ -20,13 +20,17 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'base',
+
+    ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/crm_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- contactos_conexion/models/crm_lead.py
+++ contactos_conexion/models/crm_lead.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from email.utils import formataddr
+from odoo.exceptions import UserError, RedirectWarning
+from odoo import exceptions, _
+import logging, ast
+import datetime, time
+import pytz
+import base64
+import requests
+_logger = logging.getLogger(__name__)
 
 
 class CRM(models.Model):
     _inherit = 'crm.'
     _description = 'Cambios'
+
+    def conect(self):
+        task = {"username": "apiodoo@promed-sa.com", "password": "ApiOdoo*001"}
+        resp = requests.post('https://trxk539p2e.execute-api.us-east-1.amazonaws.com/prueba/Login', json=task)
+        if resp.status_code == 200:
+            _logger.info(resp)
+            json_respuesta = resp.json()
+            _logger.info(json_respuesta)
 
 #     name = fields.Char()
 #     value = fields.Integer()
