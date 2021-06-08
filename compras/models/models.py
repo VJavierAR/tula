b973d39f7fcab49aa645dbe2419545a7fbe41bfc
Cesar <kliko1989@gmail.com>
2021-06-08 15:12:35 -0500

    vfdfvdf

--- compras/models/models.py
+++ compras/models/models.py
@@ -4,15 +4,6 @@ from odoo import models, fields, api
 
 
 class compras(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = 'account.move'
     tipo=fields.Selection(string='Tipo',[('1','Combustible'), ('2','Compras'), ('3','Pequeño Cont.'), ('4','Servicios'), ('5','Import')])
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
