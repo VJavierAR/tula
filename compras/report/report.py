eea254ea9b415c67868f5a948f567aa3ca6e43a8
Cesar <kliko1989@hotmail.com>
2021-06-18 14:23:40 -0500

    fsd

--- compras/report/report.py
+++ compras/report/report.py
@@ -20,8 +20,10 @@ class MovimientosXlsx(models.AbstractModel):
         for obj in account:
             sheet.write(i, 0, str(i-1), bold)
             sheet.write(i, 1, obj.date.strftime("%Y/%m/%d"), bold)
-            sheet.write(i, 2, obj.invoice_doc_serie, bold)
-            sheet.write(i, 3, obj.invoice_doc_number, bold)
+            #sheet.write(i, 2, obj.invoice_doc_serie, bold)
+            #sheet.write(i, 3, obj.invoice_doc_number, bold)
+            sheet.write(i, 2, '', bold)
+            sheet.write(i, 3, '', bold)
             sheet.write(i, 4, obj.partner_id.vat, bold)
             sheet.write(i, 5, obj.partner_id.name, bold)
             sheet.write(i, 6, obj.amount_untaxed if(obj.tipo=='1') else 0, bold)
