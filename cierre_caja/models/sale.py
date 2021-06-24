16d226282136cfebf055440fd289786c799a6bdb
krizaldi <kliko1989@hotmail.com>
2021-06-24 14:17:54 -0500

    fsdfsd

--- cierre_caja/models/sale.py
+++ cierre_caja/models/sale.py
@@ -6,3 +6,8 @@ class SaleLine(models.Model):
     def get_sale_order_line_multiline_description_sale(self, product):
         super(SaleLine, self).get_sale_order_line_multiline_description_sale(product)
         return product.description_sale or product.name
+
+class TypePago(models.Model):
+	_name='tipo.pago'
+	_description='Agregación de tipo de pago'
+	name=fields.Char()
