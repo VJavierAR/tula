70fa5329244dc0cec746d63e866b455500b1c211
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-06-27 22:43:03 -0500

    reparaciones

---
+++ reparaciones/models/reparaciones.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from datetime import datetime, timedelta
+import pytz
+import logging, ast
+
+_logger = logging.getLogger(__name__)
+
+
+class Reparaciones(models.Model):
+    _inherit = 'sale.order'
+    _description = 'reparaciones.reparaciones'
+
+    flota = fields.Many2one(
+        comodel_name='fleet',
+        string='Vehículo'
+    )
