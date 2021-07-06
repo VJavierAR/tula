ce546f29f187d324ef3bd1ce6cde2bc8123a925a
krizaldi <kliko1989@hotmail.com>
2021-07-06 02:23:00 -0500

    fgerrfgrerg

--- reparaciones/models/reparaciones.py
+++ reparaciones/models/reparaciones.py
@@ -68,14 +68,15 @@ class Reparaciones(models.Model):
                     pi.move_lines._action_assign()
                     pi.action_assign()
                     return pi.button_validate()
+
     @api.onchange('operations','fees_lines')
     def addLine(self):
-        for record in self:
-            arr=[]
-            data=record.operations.filtered(lambda x:x.type=='add')
-            for da in data:
-                arr.append({'product_id':da.product_id.id,'price_unit':da.price_unit,'tax_id':da.tax_id.mapped('id'),'product_uom_qty':da.product_uom_qty})
-            for da2 in record.fees_lines:
-                arr.append({'product_id':da.product_id.id,'price_unit':da.price_unit,'tax_id':da.tax_id.mapped('id'),'product_uom_qty':da.product_uom_qty})
-            record.order_line=[(5,0,0)]
-            record.order_line=arr
+        #for record in self:
+        arr=[]
+        data=self.operations.filtered(lambda x:x.type=='add')
+        for da in data:
+            arr.append({'product_id':da.product_id.id,'price_unit':da.price_unit,'tax_id':da.tax_id.mapped('id'),'product_uom_qty':da.product_uom_qty})
+        for da2 in self.fees_lines:
+            arr.append({'product_id':da.product_id.id,'price_unit':da.price_unit,'tax_id':da.tax_id.mapped('id'),'product_uom_qty':da.product_uom_qty})
+        self.order_line=[(5,0,0)]
+        self.order_line=arr
