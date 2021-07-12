697effaf31a162cb99c357e080c87a52806391bb
krizaldi <kliko1989@hotmail.com>
2021-07-12 13:16:47 -0500

    dsadsadsa

--- reparaciones/models/reparaciones.py
+++ reparaciones/models/reparaciones.py
@@ -10,7 +10,6 @@ _logger = logging.getLogger(__name__)
 
 class Reparaciones(models.Model):
     _inherit = 'sale.order'
-    _description = 'reparaciones.reparaciones'
 
     flota = fields.Many2one(comodel_name='fleet.vehicle',string='Vehículo')
     responsable = fields.Many2one(comodel_name='res.users',string='Responsable')
