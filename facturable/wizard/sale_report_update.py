1fa9a663f2b7d92a59dd188c4c5ce004cf4fb5d6
krizaldi <kliko1989@hotmail.com>
2021-07-12 11:53:00 -0500

    fsdfds

--- facturable/wizard/sale_report_update.py
+++ facturable/wizard/sale_report_update.py
@@ -13,7 +13,7 @@ class SaleReportUpdate(models.TransientModel):
 
 
 	def run(self):
-		pi=env['stock.picking'].search([['state','in',('waiting','confirmed')]],order='scheduled_date asc')
+		pi=self.env['stock.picking'].search([['state','in',('waiting','confirmed')]],order='scheduled_date asc')
 		for pick in pi:
 			pi.action_assign()
 		action = self.env.ref('sale.action_order_report_all').read()[0]
