6eefcc1d50b82cbba9207a8496150f30cf52b655
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-07-14 10:43:23 -0500

    update
    - historial de tramites
    - salto del pagina en pdf de tramite

--- facturacion/wizard/pagos_wizard.py
+++ facturacion/wizard/pagos_wizard.py
@@ -3,7 +3,7 @@ from odoo.models import TransientModel
 import logging, ast
 import datetime, time
 import pytz
-import io,csv
+import io, csv
 import datetime
 import xlsxwriter
 import base64
@@ -14,22 +14,21 @@ from odoo import exceptions, _
 from odoo.tools import email_re
 import base64
 
-
 _logger = logging.getLogger(__name__)
 
 
 class TestReport(TransientModel):
     _name = 'account.correo'
     _description = 'Tramite'
-    
+
     # _inherit = 'mail.compose.message'
-   
-    date_from = fields.Char(string='From',compute='_archivo_a',store=True)
-    date_to = fields.Char(string='Para',store=True)
-    body = fields.Char(string='body',store=True)
-    subject = fields.Char(string='subject',store=True)
-    attachment_ids=fields.Many2many('ir.attachment', string="attachment",store=True)
-    
+
+    date_from = fields.Char(string='From', compute='_archivo_a', store=True)
+    date_to = fields.Char(string='Para', store=True)
+    body = fields.Char(string='body', store=True)
+    subject = fields.Char(string='subject', store=True)
+    attachment_ids = fields.Many2many('ir.attachment', string="attachment", store=True)
+
     def _default_move_ids(self):
         return self.env['account.move'].browse(self.env.context.get('active_ids'))
 
@@ -41,8 +40,8 @@ class TestReport(TransientModel):
     )
 
     @api.onchange('date_from')
-    def _archivo_a(self):        
-        pdf = self.env.ref('facturacion.reporte_seguimiento')       
+    def _archivo_a(self):
+        pdf = self.env.ref('facturacion.reporte_seguimiento')
 
         pdf = self.env.ref('facturacion.reporte_de_seguimiento').sudo().render_qweb_pdf([self.move_ids[0].id])[0]
         a = self.env['ir.attachment'].create({
@@ -52,52 +51,40 @@ class TestReport(TransientModel):
             'res_model': 'account.correo',
             'datas': base64.b64encode(pdf),
             'mimetype': 'application/x-pdf',
-            
+
         })
-        
-        self.date_to=str(self.move_ids[0].partner_id.correoFac)
-        self.date_from=''
-        self.subject='Reporte de seguimiento .'
-        self.body="<br>Estimado  "+str(self.move_ids[0].partner_id.name)+",</br>, se entregan facturas tramitadas."
-        self.attachment_ids=[(6,0,[a.id])]
-    
-    
-    
 
-    
-    
-    
-    
+        self.date_to = str(self.move_ids[0].partner_id.correoFac)
+        self.date_from = ''
+        self.subject = 'Reporte de seguimiento .'
+        self.body = "<br>Estimado  " + str(
+            self.move_ids[0].partner_id.name) + ",</br>, se entregan facturas tramitadas."
+        self.attachment_ids = [(6, 0, [a.id])]
 
-
-
-        
-        
-        
-        
-
-        
-    
-
-    def mensaje(self):       
-        pal=''
-        re=[]        
+    def mensaje(self):
+        pal = ''
+        re = []
         mail_template = self.env.ref('facturacion.reporte_seguimiento')
-        clientes=[]
-        for move in self.move_ids:                                                            
+        clientes = []
+        for move in self.move_ids:
             clientes.append(move.partner_id.id)
-            move.write({'tramite':'tramitadas'})
-        finalL=set(clientes)    
+            move.write({'tramite': 'tramitadas'})
+            if move.partner_id.id not in clientes:
+                self.env['facturas.tramites'].create({
+                    'tramite_file': self.attachment_ids,
+                    'factura': move.id,
+                    'clientes': move.partner_id
+                })
+        finalL = set(clientes)
         cli = self.env['res.partner']
-        
+
         for send in finalL:
             cliente = cli.search([('id', '=', send)])
-            vals={
-                    'email_to': self.date_to,
-                    'body_html': self.body,
-                    'attachment_ids': self.attachment_ids,
-                    'subject': self.subject
+            vals = {
+                'email_to': self.date_to,
+                'body_html': self.body,
+                'attachment_ids': self.attachment_ids,
+                'subject': self.subject
             }
             mail_id = self.env['mail.mail'].create(vals)
             mail_id.send()
-
