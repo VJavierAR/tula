723eea55145962b26b4c7f328e14a503421a4d31
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-07-18 22:59:04 -0500

    Update models.py

--- crm_l/models/models.py
+++ crm_l/models/models.py
@@ -317,28 +317,16 @@ class Crm_l(models.Model):
 
         return values
 
-    def convert_opportunity(self, partner_id, user_ids=False, team_id=False):
-        customer = False
-        if partner_id:
-            customer = self.env['res.partner'].browse(partner_id)
-        for lead in self:
-            if not lead.active or lead.probability == 100:
-                continue
-            vals = lead._convert_opportunity_data(customer, team_id)
-            vals = self.cambia_tipo(vals)
-            _logger.info("valssss:::     " + str(vals))
-            lead.write(vals)
 
-        if user_ids or team_id:
-            self.handle_salesmen_assignment(user_ids, team_id)
-"""
 class Iniciativa2Oportunidad(models.TransientModel):
     _inherit = 'crm.lead2opportunity.partner'
 
     def action_apply(self):
         result = super(Iniciativa2Oportunidad, self).action_apply()
 
-
+        _logger.info("result: " + str(result))
+        result.test()
+        _logger.info("result: " + str(result))
 
         return result
-"""
+
