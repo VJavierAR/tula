5331e1f439b87188c6cbbc804b580c5e6d662041
krizaldi <kliko1989@hotmail.com>
2021-07-23 12:58:42 -0500

    dff

--- cierre_caja/models/invoice.py
+++ cierre_caja/models/invoice.py
@@ -63,7 +63,7 @@ class CierreLineas(models.Model):
         for line in self:
             pagos = line.cierre_id.pagos_hoy
             line.monto_calculado = sum(pagos.filtered(lambda p: p.journal_id == line.name).mapped('monto_moneda'))
-            line.diferencia = line.monto_calculado - line.monto_reportado
+            line.diferencia = round((line.monto_calculado - line.monto_reportado),2)
     
     @api.depends('name')
     def compute_monto_final(self):
