34553f93591460f8600bb087f93aa779e91f8c9d
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-07-25 13:46:57 -0500

    Update crm_lead.py
    - Cambio de "Entidad" a "Unidad de compra" al cargar datos de panamacompra para el nombre de empresa en odoo.
    - Corrección a botón que permite agregar días para las pruebas

--- contactos_conexion/models/crm_lead.py
+++ contactos_conexion/models/crm_lead.py
@@ -82,21 +82,25 @@ class CRM(models.Model):
 
     def agrega_dias_write_date(self):
         self.conexis = True
-        date_1 = (datetime.datetime.strptime(self.write_date.strftime("%m-%d-%Y %H:%M:%S"), '%m-%d-%Y %H:%M:%S') + relativedelta(days=+ 15))
+        date_1 = (datetime.datetime.strptime(self.write_date.strftime("%m-%d-%Y %H:%M:%S"),
+                                             '%m-%d-%Y %H:%M:%S') + relativedelta(days=+ 15))
         _logger.info("date_1: " + str(date_1))
         self.env.cr.execute("update crm_lead set write_date = '" + str(date_1) + "' where  id = " + str(self.id) + ";")
+        self.env.cr.commit()
 
     def agrega_meses_write_date(self):
         date_1 = (datetime.datetime.strptime(self.write_date.strftime("%m-%d-%Y %H:%M:%S"),
                                              '%m-%d-%Y %H:%M:%S') + relativedelta(days=+ 180))
         _logger.info("date_1: " + str(date_1))
         self.env.cr.execute("update crm_lead set write_date = '" + str(date_1) + "' where  id = " + str(self.id) + ";")
+        self.env.cr.commit()
 
     def decrmenta_dias_write_date(self):
         date_1 = (datetime.datetime.strptime(self.write_date.strftime("%m-%d-%Y %H:%M:%S"),
                                              '%m-%d-%Y %H:%M:%S') + relativedelta(days=- 15))
         _logger.info("date_1: " + str(date_1))
         self.env.cr.execute("update crm_lead set write_date = '" + str(date_1) + "' where  id = " + str(self.id) + ";")
+        self.env.cr.commit()
 
 
 """
