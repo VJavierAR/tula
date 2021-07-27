4f927e039de396391eb927834dd0ee19970cba78
krizaldi <kliko1989@hotmail.com>
2021-07-27 15:13:27 -0500

    fsdfsd

--- cierre_caja/models/invoice.py
+++ cierre_caja/models/invoice.py
@@ -281,8 +281,6 @@ class Cierre(models.Model):
         descuento2=0
         iva2=0
         moneda=self.company_id.currency_id.id
-            
-                rec.monto_moneda=(rec.amount/)
         for li in lines:
             for liin in li.invoice_line_ids:
                 total=total+(liin.price_unit*liin.quantity)
