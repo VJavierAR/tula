f7b9feb2001ca93346633653a25c1647c1c97f49
krizaldi <kliko1989@hotmail.com>
2021-07-30 13:59:05 -0500

    dasdasd

--- cierre_caja/models/invoice.py
+++ cierre_caja/models/invoice.py
@@ -363,7 +363,7 @@ class Cierre(models.Model):
         facturas_ayer=self.env['account.move.line'].search([['date','<',fecha],['move_id.state', '=', 'posted']])
         facturas_hoy=self.env['account.move.line'].search([['date','=',fecha],['move_id.state', '=', 'posted']])
         for jo in j:
-            ayer=sum(facturas_ayer.filtered(lambda x:x.account_id.id=jo.default_debit_account_id.id or x.account_id.id==jo.default_credit_account_id.id).mapped('balance'))
+            ayer=sum(facturas_ayer.filtered(lambda x:x.account_id.id==jo.default_debit_account_id.id or x.account_id.id==jo.default_credit_account_id.id).mapped('balance'))
             hoy_haber=(sum(facturas_hoy.filtered(lambda x:x.account_id.id==jo.default_credit_account_id.id).mapped('credit')))
             hoy_deber=(sum(facturas_hoy.filtered(lambda x:x.account_id.id==jo.default_debit_account_id.id).mapped('debit')))
             data.append([jo.name,"{:,}".format(ayer),"{:,}".format(hoy_deber),"{:,}".format(hoy_haber),"{:,}".format(ayer-hoy_deber+hoy_haber)])
