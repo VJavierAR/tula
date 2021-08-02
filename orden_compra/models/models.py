a93c2b18541833ef83b3f3132d5945c9f2c8c4b9
krizaldi <kliko1989@hotmail.com>
2021-08-02 01:37:13 -0500

    hjghjhgh

--- orden_compra/models/models.py
+++ orden_compra/models/models.py
@@ -14,6 +14,7 @@ class Factura(models.Model):
 			raise UserError(_("A payment journal entry generated in a journal configured to post entries only when payments are reconciled with a bank statement cannot be manually posted. Those will be posted automatically after performing the bank reconciliation."))
 		if self.env.context.get('default_type'):
 			context = dict(self.env.context)
+			_logger.info(context['default_type'])
 			del context['default_type']
 			self = self.with_context(context)
 		return self.post()
