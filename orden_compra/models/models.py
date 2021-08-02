260c859b70e1fcfee7b0654d9d8913b679dfa5b3
krizaldi <kliko1989@hotmail.com>
2021-08-02 13:36:36 -0500

    efewfew

--- orden_compra/models/models.py
+++ orden_compra/models/models.py
@@ -53,12 +53,12 @@ class Factura(models.Model):
 			    raise UserError(_('You cannot modify a posted entry of this journal because it is in strict mode.'))
 			# We remove all the analytics entries for this journal
 			move.mapped('line_ids.analytic_line_ids').unlink()
-		self.purchase_id.button_cancel()
+		p=self.purchase_id.button_cancel()
 		self.purchase_id.picking_ids.unlink()
 		self.purchase_id.unlink()
 		self.mapped('line_ids').remove_move_reconcile()
 		self.write({'state': 'draft','invoice_origin':''})
-
+		return p
 
 class LinesFactura(models.Model):
 	_inherit='account.move.line'
