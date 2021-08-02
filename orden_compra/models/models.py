6f374cdf3a98237f27458e6300c8065a548a1b12
krizaldi <kliko1989@hotmail.com>
2021-08-02 01:09:19 -0500

    bfgfg

--- orden_compra/models/models.py
+++ orden_compra/models/models.py
@@ -7,6 +7,15 @@ class Factura(models.Model):
 	_inherit='account.move'
 
 
+	def action_post(self):
+		if self.filtered(lambda x: x.journal_id.post_at == 'bank_rec').mapped('line_ids.payment_id').filtered(lambda x: x.state != 'reconciled'):
+			raise UserError(_("A payment journal entry generated in a journal configured to post entries only when payments are reconciled with a bank statement cannot be manually posted. Those will be posted automatically after performing the bank reconciliation."))
+		if self.env.context.get('default_type'):
+			context = dict(self.env.context)
+			del context['default_type']
+			self = self.with_context(context)
+		return self.post()
+
 
 
 class LinesFactura(models.Model):
