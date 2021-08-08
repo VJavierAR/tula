e549ed35b97959b9eb77e5bc0e2a2f63e696b74e
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-08-08 12:59:12 -0500

    Update stock_picking.py

--- orden_abierta/models/stock_picking.py
+++ orden_abierta/models/stock_picking.py
@@ -28,14 +28,51 @@ class StockPicking(models.Model):
             [("name", "=", "Notificaciones de ordenes abiertas")]).mapped('users')
 
         for orden in ordenes_entrega:
+
+            message_text = "Se necesita validar orden " + orden.name
+            # odoo runbot
+            odoobot_id = self.env['ir.model.data'].sudo().xmlid_to_res_id("base.partner_root")
+
             notification_ids = []
+            notificaciones = [(4, self.env.user.partner_id.id), (4, odoobot_id)]
             for usuario in usuarios:
                 notification_ids.append((0, 0, {
                     'res_partner_id': usuario.partner_id.id,
                     'notification_type': 'inbox'}))
+                notificaciones.append([4, usuario.partner_id.id])
+            """
             orden.message_post(
                 body='Se necesita validar orden',
                 message_type='notification',
                 subtype='mail.mt_comment',
                 author_id=2,
                 notification_ids=notification_ids)
+            """
+
+
+            # find if a channel was opened for this user before
+            channel = self.env['mail.channel'].sudo().search([
+                ('name', '=', 'Picking Validated'),
+                ('channel_partner_ids', 'in', [self.env.user.partner_id.id])
+            ],
+                limit=1,
+            )
+
+            if not channel:
+                # create a new channel
+                channel = self.env['mail.channel'].with_context(mail_create_nosubscribe=True).sudo().create({
+                    'channel_partner_ids': notificaciones,
+                    'public': 'private',
+                    'channel_type': 'chat',
+                    'email_send': False,
+                    'name': f'Picking Validated',
+                    'display_name': f'Picking Validated',
+                })
+
+            # send a message to the related user
+            channel.sudo().message_post(
+                body=message_text,
+                author_id=odoobot_id,
+                message_type="comment",
+                subtype="mail.mt_comment",
+            )
