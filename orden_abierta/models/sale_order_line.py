a4d05b970a1b01926d06e13c3ecce9ecf75bd48f
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-08-08 02:37:49 -0500

    Update sale_order_line.py

--- orden_abierta/models/sale_order_line.py
+++ orden_abierta/models/sale_order_line.py
@@ -54,33 +54,54 @@ class SaleOrderLineOrdenAbierta(models.Model):
                 if self.order_partner_id.id == codigo.cliente.id:
                     self.codigo_cliente = codigo.codigo_producto
 
+
             # Cantidad reservada
-            self.obtener_cantidad_reservada()
+            estados_no_aprobados = ['draft', 'sent']
+            ordenes = self.env['sale.order'].search(
+                [
+                    ('state', 'in', estados_no_aprobados),
+                    ('es_orden_abierta', '=', True)
+                ])
+            cantidad_dispobible = self.product_id.qty_available
+            cantidad_reservada_suma = 0
+            for orden in ordenes:
+                for linea in orden.order_line:
+                    if linea.product_id.id == self.product_id.id:
+                        cantidad_reservada_suma += linea.product_uom_qty
+
+            if cantidad_reservada_suma > 0:
+                self.cantidad_reservada = cantidad_dispobible - cantidad_reservada_suma
+            else:
+                self.cantidad_reservada = 0
+
 
             # Mensaje inventario actual
-            self.mensaje_inventario_actual()
-
-    def obtener_cantidad_reservada(self):
-        estados_no_aprobados = ['draft', 'sent']
-        ordenes = self.env['sale.order'].search(
-            [
-                ('state', 'in', estados_no_aprobados),
-                ('es_orden_abierta', '=', True)
-            ])
-        cantidad_dispobible = self.product_id.qty_available
-        cantidad_reservada_suma = 0
-        for orden in ordenes:
-            for linea in orden.order_line:
-                if linea.product_id.id == self.product_id.id:
-                    cantidad_reservada_suma += linea.product_uom_qty
-
-        if cantidad_reservada_suma > 0:
-            self.cantidad_reservada = cantidad_dispobible - cantidad_reservada_suma
-        else:
-            self.cantidad_reservada = 0
-
-    def mensaje_inventario_actual(self):
-        mensaje = "Plane vender "
+            cantidad_a_vender = self.product_uom_qty
+            cantidad_prevista = self.product_id.virtual_available
+            cantidad_pedidos_abiertos = cantidad_reservada_suma
+            cantidad_disponible_cantidad_pa = cantidad_disponible - cantidad_pedidos_abiertos
+            if cantidad_a_vender > cantidad_disponible_cantidad_pa:
+                nombre_producto = self.product_id.name
+                almacenes_stock = self.product_id.stock_quant_ids.filtered(
+                    lambda x:
+                    x.company_id.id is not False and
+                    x.quantity >= 0 and
+                    x.location_id.usage == 'internal'
+                )
+                nombre_almacen = ""
+                for data in almacenes_stock:
+                    nombre_almacen += str(data.location_id.display_name) + ": " + str(data.quantity) + "\n"
+                mensaje = "Planea vender " + str(
+                    cantidad_a_vender) + " de " + nombre_producto + " pero solo tiene " + str(cantidad_disponible)
+                mensaje += " en los siguientes almacenes:\nAlmacén: cantidad" + nombre_almacen + "\n\n"
+                mensaje += "Existen " + str(cantidad_disponible_cantidad_pa)
+                mensaje += " disponibles (Cantidad a mano, menos la cantidad de pedidos abiertos)."
+                return {
+                    'warning': {
+                        'title': _('Inventario actual!'),
+                        'message': _(mensaje),
+                    },
+                }
 
     # @api.multi
     def dup_line_to_order(self, order_id=None):
