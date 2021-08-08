dd320115059e1851d57134d676b03df2e61cdc8b
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-08-08 13:49:29 -0500

    Update stock_picking.py

--- orden_abierta/models/stock_picking.py
+++ orden_abierta/models/stock_picking.py
@@ -19,20 +19,57 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
     _description = 'cron'
 
+    def write(self, vals):
+        if 'state' in vals and vals['state'] == 'done' and 'out' in self.name and self.sale_id.id and self.sale_id.user_id.id:
+            vendedor_id = self.sale_id.user_id.id
+            message_text = "Se necesita validar orden " + self.sale_id.name
+            # odoo runbot
+            odoobot_id = self.env['ir.model.data'].sudo().xmlid_to_res_id("base.partner_root")
+            notificaciones = [(4, self.env.user.partner_id.id), (4, odoobot_id), (4, vendedor_id)]
+            # find if a channel was opened for this user before
+            channel = self.env['mail.channel'].sudo().search([
+                ('name', '=', 'Alerta vendedores'),
+                ('channel_partner_ids', 'in', [self.env.user.partner_id.id])
+            ],
+                limit=1,
+            )
+
+            if not channel:
+                # create a new channel
+                channel = self.env['mail.channel'].with_context(mail_create_nosubscribe=True).sudo().create({
+                    'channel_partner_ids': notificaciones,
+                    'public': 'private',
+                    'channel_type': 'chat',
+                    'email_send': False,
+                    'name': f'Alerta vendedores',
+                    'display_name': f'Alerta vendedores',
+                })
+
+            # send a message to the related user
+            channel.sudo().message_post(
+                body=message_text,
+                author_id=odoobot_id,
+                message_type="comment",
+                subtype="mail.mt_comment",
+            )
+
+        result = super(StockPicking, self).create(vals)
+        return result
+
     @api.onchange('state')
     def cambia_state(self):
         if self.state:
-            if self.state == 'done' and 'out' in self.name and self.sale_id.id:
-                usuarios = self.env['res.groups'].sudo().search(
-                    [("name", "=", "Notificacion a vendedor al validar picking")]).mapped('users')
+            if self.state == 'done' and 'out' in self.name and self.sale_id.id and self.sale_id.user_id.id:
+                # usuarios = self.env['res.groups'].sudo().search(
+                #    [("name", "=", "Notificacion a vendedor al validar picking")]).mapped('users')
                 vendedor_id = self.sale_id.user_id.id
 
-                message_text = "Se necesita validar orden " + orden.name
+                message_text = "Se necesita validar orden " + self.sale_id.name
                 # odoo runbot
                 odoobot_id = self.env['ir.model.data'].sudo().xmlid_to_res_id("base.partner_root")
-                notificaciones = [(4, self.env.user.partner_id.id), (4, odoobot_id)]
-                for usuario in usuarios:
-                    notificaciones.append([4, usuario.partner_id.id])
+                notificaciones = [(4, self.env.user.partner_id.id), (4, odoobot_id), (4, vendedor_id)]
+                # for usuario in usuarios:
+                #    notificaciones.append([4, usuario.partner_id.id])
 
                 # find if a channel was opened for this user before
                 channel = self.env['mail.channel'].sudo().search([
