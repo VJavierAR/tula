5ef8f650edd544f5c148e035171fa2dbf0f825dc
krizaldi <kliko1989@hotmail.com>
2021-08-12 12:55:43 -0500

    gddfdgdf

--- orden_compra/models/models.py
+++ orden_compra/models/models.py
@@ -127,7 +127,7 @@ class LinesFactura(models.Model):
 			if(record.product_id.id!=False):
 				if(record.nuevo_precio!=record.valorX):
 					#record.nueva_utilidad=((record.nuevo_precio-record.nuevo_costo)/record.nuevo_precio)*100 if(record.nuevo_precio!=0) else 0
-					record.nueva_utilidad=((record.nuevo_precio-nuevo_costo)*100)/record.nuevo_costo if(record.nuevo_costo!=0) else 0
+					record.nueva_utilidad=((record.nuevo_precio-record.nuevo_costo)*100)/record.nuevo_costo if(record.nuevo_costo!=0) else 0
 				if(record.utilida!=record.nueva_utilidad):
 					newprice=(record.nuevo_costo * record.nueva_utilidad / 100) + record.nuevo_costo
 					record.nuevo_precio=newprice
