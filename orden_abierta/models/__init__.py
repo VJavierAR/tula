f9b0513a2f4f14e41b876f5bc2233c632698d680
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-08-20 01:46:10 -0500

    Update __init__.py

--- orden_abierta/models/__init__.py
+++ orden_abierta/models/__init__.py
@@ -2,8 +2,8 @@
 
 from . import product
 from . import pedido_abiert_linea
-from . import sale
 from . import sale_order_line
+from . import sale
 from . import pedido_abierto
 from . import res_partner
 from . import stock_picking
