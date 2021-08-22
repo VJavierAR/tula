5412b9d967375ddc89468ee92fb40bb7deccc0b0
Cesar <kliko1989@hotmail.com>
2021-08-21 20:27:16 -0500

    swsw

--- orden_compra/models/models.py
+++ orden_compra/models/models.py
@@ -107,7 +107,7 @@ class LinesFactura(models.Model):
 	def _compute_amount(self):
 		for line in self:
 			price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-			taxes = line.tax_ids.compute_all(price, line.order_id.currency_id, 1, product=line.product_id, partner=line.move_id.partner_id)
+			taxes = line.tax_ids.compute_all(price, line.move_id.currency_id, 1, product=line.product_id, partner=line.move_id.partner_id)
 			line.update({
 			    'impuesto': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
 			    #'price_total': taxes['total_included'],
