064daabb6ba28dfdfef398e0d44731d4717783a0
krizaldi <kliko1989@hotmail.com>
2021-08-24 15:43:44 -0500

    dasdsa

--- OLA/models/studio_code.py
+++ OLA/models/studio_code.py
@@ -91,11 +91,12 @@ class ProductProduct(models.Model):
 
 	def write(self, vals):
 		precio=vals['nuevo_costo_facturacion_impuesto'] if('nuevo_costo_facturacion_impuesto' in vals) else self.nuevo_costo_facturacion_impuesto
+		_logger.info("self.id: " + str(precio))
 		if 'standard_price' in vals and precio==0:
-			#_logger.info("self.id: " + str(self.id))
+			_logger.info("self.id: " + str(self.id))
 			producto = self.env['product.template'].search([('id', '=', self.product_tmpl_id.id)])
-			#_logger.info("producto: " + str(producto))
-			# actualiza precio de venta
+			_logger.info("producto: " + str(producto))
+			actualiza precio de venta
 			coste = vals['standard_price']
 			vals['list_price'] = (coste * producto.x_studio_utilidad_precio_de_venta / 100) + coste
 			producto.list_price = (coste * producto.x_studio_utilidad_precio_de_venta / 100) + coste
@@ -105,12 +106,12 @@ class ProductProduct(models.Model):
 
 			vals['x_studio_utilidad_precio_de_venta'] = producto.x_studio_utilidad_precio_de_venta
 			vals['x_studio_utilidad_'] = producto.x_studio_utilidad_
-			#_logger.info(
-			#	"standard_price: " + str(coste) +
-			#	" self.x_studio_utilidad_precio_de_venta: " + str(producto.x_studio_utilidad_precio_de_venta) +
-			#	" self.x_studio_utilidad_: " + str(producto.x_studio_utilidad_) +
-			#	" vals: " + str(vals)
-			#)
+			_logger.info(
+				"standard_price: " + str(coste) +
+				" self.x_studio_utilidad_precio_de_venta: " + str(producto.x_studio_utilidad_precio_de_venta) +
+				" self.x_studio_utilidad_: " + str(producto.x_studio_utilidad_) +
+				" vals: " + str(vals)
+			)
 		res = super(ProductProduct, self).write(vals)
 		#_logger.info("res: " + str(res))
 		return res
