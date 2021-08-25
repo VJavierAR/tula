84d0390512f9d445ea52ba69213d70492e9d38fd
krizaldi <kliko1989@hotmail.com>
2021-08-25 05:20:42 -0500

    juyjuy

--- OLA/models/studio_code.py
+++ OLA/models/studio_code.py
@@ -71,31 +71,31 @@ class ProductProduct(models.Model):
 		if('list_price' in vals):
 			if(vals['list_price']==0):
 				del vals['list_price']
-		check=vals['check'] if('check' in vals) else False
+		#check=vals['check'] if('check' in vals) else False
 		if('nuevo_costo_facturacion_impuesto' in vals):
 			if(vals['nuevo_costo_facturacion_impuesto']==0 and check!=True):
 				del vals['nuevo_costo_facturacion_impuesto']
-		precio=vals['nuevo_costo_facturacion_impuesto'] if('nuevo_costo_facturacion_impuesto' in vals) else self.nuevo_costo_facturacion_impuesto
-		if 'standard_price' in vals and precio==0:
-			#_logger.info("self.id: " + str(self.id))
-			producto = self.env['product.template'].search([('id', '=', self.product_tmpl_id.id)])
-			#_logger.info("producto: " + str(producto))
-			# actualiza precio de venta
-			coste = vals['standard_price']
-			vals['list_price'] = (coste * producto.x_studio_utilidad_precio_de_venta / 100) + coste
-			producto.list_price = (coste * producto.x_studio_utilidad_precio_de_venta / 100) + coste
-			# actualiza precio minimo
-			vals['x_studio_precio_mnimo'] = (coste * producto.x_studio_utilidad_ / 100) + coste
-			producto.x_studio_precio_mnimo = (coste * producto.x_studio_utilidad_ / 100) + coste
+		# precio=vals['nuevo_costo_facturacion_impuesto'] if('nuevo_costo_facturacion_impuesto' in vals) else self.nuevo_costo_facturacion_impuesto
+		# if 'standard_price' in vals and precio==0:
+		# 	#_logger.info("self.id: " + str(self.id))
+		# 	producto = self.env['product.template'].search([('id', '=', self.product_tmpl_id.id)])
+		# 	#_logger.info("producto: " + str(producto))
+		# 	# actualiza precio de venta
+		# 	coste = vals['standard_price']
+		# 	vals['list_price'] = (coste * producto.x_studio_utilidad_precio_de_venta / 100) + coste
+		# 	producto.list_price = (coste * producto.x_studio_utilidad_precio_de_venta / 100) + coste
+		# 	# actualiza precio minimo
+		# 	vals['x_studio_precio_mnimo'] = (coste * producto.x_studio_utilidad_ / 100) + coste
+		# 	producto.x_studio_precio_mnimo = (coste * producto.x_studio_utilidad_ / 100) + coste
 
-			vals['x_studio_utilidad_precio_de_venta'] = producto.x_studio_utilidad_precio_de_venta
-			vals['x_studio_utilidad_'] = producto.x_studio_utilidad_
-			#_logger.info(
-			#	"standard_price: " + str(coste) +
-			#	" self.x_studio_utilidad_precio_de_venta: " + str(producto.x_studio_utilidad_precio_de_venta) +
-			#	" self.x_studio_utilidad_: " + str(producto.x_studio_utilidad_) +
-			#	" vals: " + str(vals)
-			#)
+		# 	vals['x_studio_utilidad_precio_de_venta'] = producto.x_studio_utilidad_precio_de_venta
+		# 	vals['x_studio_utilidad_'] = producto.x_studio_utilidad_
+		# 	#_logger.info(
+		# 	#	"standard_price: " + str(coste) +
+		# 	#	" self.x_studio_utilidad_precio_de_venta: " + str(producto.x_studio_utilidad_precio_de_venta) +
+		# 	#	" self.x_studio_utilidad_: " + str(producto.x_studio_utilidad_) +
+		# 	#	" vals: " + str(vals)
+		# 	#)
 		res = super(ProductProduct, self).write(vals)
 		#_logger.info("res: " + str(res))
 		return res
