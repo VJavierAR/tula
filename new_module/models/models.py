6fd1cdb41c542b107192e6571ca023d32a24dec6
Javier Monroy <javv.monroy@gmail.com>
2021-09-01 16:01:39 -0500

    modelo
    fasdfasdffa

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class new_module(models.Model):
-#     _name = 'new_module.new_module'
-#     _description = 'new_module.new_module'
-
-#     name = fields.Char()
-#     value = fields.Integer()
+class new_module(models.Model):
+    _name = 'new_module.new_module'
+    _inherit = ['res.partner'] 
+    _description = 'new_module.new_module'
+    
+    name = fields.Char()
+    campo = fields.Text(
+         string="Campo de prueba",
+         store=True 
+    )
 #     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
+     description = fields.Text()
+
 #     @api.depends('value')
 #     def _value_pc(self):
 #         for record in self:
