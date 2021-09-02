fa973d99ceb12478056134b502a502fc03b69982
Javier Monroy <javv.monroy@gmail.com>
2021-09-02 18:28:50 -0500

    cambios de ayer
    solo es lo de ayer

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -4,19 +4,19 @@ from odoo import models, fields, api
 
 
 class new_module(models.Model):
-    _name = 'new_module.new_module'
-    _inherit = ['res.partner'] 
-    _description = 'new_module.new_module'
+     _name = 'new_module.new_module'
+     #_inherit = ['res.partner'] 
+     _description = 'new_module.new_module'
     
-    name = fields.Char()
-    campo = fields.Text(
-         string="Campo de prueba",
-         store=True 
-    )
-#     value2 = fields.Float(compute="_value_pc", store=True)
+     name = fields.Char()
+     campo = fields.Text(
+     string="Campo de prueba",
+     store=True 
+     )
+     #     value2 = fields.Float(compute="_value_pc", store=True)
      description = fields.Text()
 
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+     #     @api.depends('value')
+     #     def _value_pc(self):
+     #         for record in self:
+     #             record.value2 = float(record.value) / 100
