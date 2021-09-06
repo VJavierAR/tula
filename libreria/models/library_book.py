0279beb9ef6d6571922aa8fc6db7789016227179
Javier Monroy <javv.monroy@gmail.com>
2021-09-05 19:00:30 -0500

    vista quitada de libreria

--- libreria/models/library_book.py
+++ libreria/models/library_book.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 #comentario de control
 class LibraryBook(models.Model):
     _name = 'library.book'
-
+    _description='library.book'
     name = fields.Char('Title', required=True)
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many(
