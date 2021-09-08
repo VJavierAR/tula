a89f6f65e59a09b2cb4bc1db78d93453d373dd67
Javier Monroy <javv.monroy@gmail.com>
2021-09-07 20:18:35 -0500

    un commit3

--- libreria/__manifest__.py
+++ libreria/__manifest__.py
@@ -25,8 +25,8 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        #'views/library_book.xml',
+        #'views/views.xml',
+        'views/library_book.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
