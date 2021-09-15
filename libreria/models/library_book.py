db6d42d0969cbd8647e268a326f1a38d4fbaa328
Javier Monroy <javv.monroy@gmail.com>
2021-09-14 20:54:46 -0500

    quitada compia modelo2

--- libreria/models/library_book.py
+++ libreria/models/library_book.py
@@ -174,11 +174,11 @@ class LibraryBook(models.Model):
     @api.model
     def is_allowed_transition(self, old_state,new_state):
         allowed = [('draft','available'),
-        ('available','borrowed'),
-        ('borrowed','available'),
-        ('available','lost'),
-        ('borrowed','lost'),
-        ('lost','available')]
+            ('available','borrowed'),
+            ('borrowed','available'),
+            ('available','lost'),
+            ('borrowed','lost'),
+            ('lost','available')]
         return (old_state,new_state) in allowed
     
     def change_state(self, new_state):
